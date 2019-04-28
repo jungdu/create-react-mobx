8eb1540c8c76189eb0c87423d128dc1d241e5de9
fred <jungdujang@gmail.com>
2019-04-28 17:20:48 +0900

    Sample Component 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {observable, action} from 'mobx'
+import {observer} from 'mobx-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@observer
+class App extends Component {
+
+  @observable count = 0;
+
+  @action
+  handleIncreaseClick = () => {
+    this.count = this.count + 1;
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <p> count : {this.count} </p>
+        <p> <button onClick={this.handleIncreaseClick}>INCREASE</button> </p>
+      </div>
+    );
+  }
 }
 
 export default App;
